010d0c250f08a7f60b6db6b0b8dda3cea34ec378
Арам Григорян <a.grigoryan@energochain.ru>
2021-10-20 18:47:51 +0300

    wip2

--- Testfile.py
+++ Testfile.py
@@ -1,49 +1,47 @@
 import unittest
-from main import CalcFigures
 
-class TestCalc(unittest.TestCase):
 
-    def setUp(self):
-        self.calcfigures = CalcFigures()
+def test_choosetracks(self):
+    self.assertEqual(Music.choosetracks(20), 'error')
+    self.assertEqual(Music.choosetracks(10), 'error')
+    self.assertEqual(Music.choosetracks(0), 'error')
+    self.assertEqual(Music.choosetracks(1), 'error')
 
-    def test_square_quad(self):
-        self.assertEqual(self.calcfigures.square_quad(4), 16)
+def test_bassboost(self):
+    self.assertEqual(Music.bassboost(20), 'error')
+    self.assertEqual(Music.bassboost(10), 'error')
+    self.assertEqual(Music.bassboost(0), 'error')
+    self.assertEqual(Music.bassboost(1), 'error')
 
-    def test_square_quad(self):
-        self.assertEqual(self.calcfigures.square_quad(-4), "Неверный ввод")
+def test_speedup(self):
+    self.assertEqual(Music.speedup(20), 'error')
+    self.assertEqual(Music.speedup(10), 'error')
+    self.assertEqual(Music.speedup(0), 'error')
+    self.assertEqual(Music.speedup(1), 'error')
 
-    def test_square_quad(self):
-        self.assertEqual(self.calcfigures.square_quad(0), "Неверный ввод")
+def test_mergetracks(self):
+    self.assertEqual(Music.mergetracks(20), 'error')
+    self.assertEqual(Music.mergetracks(10), 'error')
+    self.assertEqual(Music.mergetracks(0), 'error')
+    self.assertEqual(Music.mergetracks(1), 'error')
 
-    def test_square_circle(self):
-        self.assertEqual(self.calcfigures.square_circle(2), 12.566)
+def test_outputname(self):
+    self.assertEqual(Music.speedup(20), 'error')
+    self.assertEqual(Music.speedup(10), 'error')
+    self.assertEqual(Music.speedup(0), 'error')
+    self.assertEqual(Music.speedup(1), 'error')
 
-    def test_square_rectangle(self):
-        self.assertEqual(self.calcfigures.square_rectangle(-2, 4), "Неверный ввод")
+def test_exporttrack(self):
+    self.assertEqual(Music.exporttrack(20), 'error')
+    self.assertEqual(Music.exporttrack(10), 'error')
+    self.assertEqual(Music.exporttrack(0), 'error')
+    self.assertEqual(Music.exporttrack(1), 'error')
 
-    def test_square_rectangle(self):
-        self.assertEqual(self.calcfigures.square_rectangle(0, 4), "Неверный ввод")
-
-    def test_square_rectangle(self):
-        self.assertEqual(self.calcfigures.square_rectangle(2, -4), "Неверный ввод")
-
-    def test_square_rectangle(self):
-        self.assertEqual(self.calcfigures.square_rectangle(3, 0), "Неверный ввод")
-
-    def test_square_triangle(self):
-        self.assertEqual(self.calcfigures.square_triangle(-3, 8), "Неверный ввод")
-
-    def test_square_triangle(self):
-        self.assertEqual(self.calcfigures.square_triangle(3, -8), "Неверный ввод")
-
-    def test_quare_parallelogram(self):
-        self.assertEqual(self.calcfigures.square_parallelogram(5, 7), 35)
-
-    def test_quare_parallelogram(self):
-        self.assertEqual(self.calcfigures.square_parallelogram(-5, 7), "Неверный ввод")
-
-    def test_quare_parallelogram(self):
-        self.assertEqual(self.calcfigures.square_parallelogram(5, -7), "Неверный ввод")
+def test_choosingoption(self):
+    self.assertEqual(Music.choosingoption(20), 'error')
+    self.assertEqual(Music.choosingoption(10), 'error')
+    self.assertEqual(Music.choosingoption(0), 'error')
+    self.assertEqual(Music.choosingoption(1), 'error')
 
 if __name__ == 'main':
     unittest.main()
